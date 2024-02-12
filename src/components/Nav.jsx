b5f031d80148140b614d46f66f702e359cebb214
Wyzxk <tkg.jhoncaro@gmail.com>
2024-02-11 23:07:22 -0500

    logo eliminated

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,15 +5,6 @@ function Navbar() {
   return (
     <nav className="p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/">
-          <div className="flex items-center">
-            <img
-              src="/logo_idiomas_eafit.png" // Ruta de la imagen
-              alt="Logo" // Texto alternativo de la imagen
-              className="h-7" // Clase de Tailwind para definir el ancho de la imagen
-            />
-          </div>
-        </Link>
         <div>
           <ul className="flex space-x-4">
             <li>
